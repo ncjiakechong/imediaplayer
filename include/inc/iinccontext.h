51dff2295bd55f3c675554eaf8c0875d4ed3bd07
jiakechong <ncjiakechong@gmail.com>
2025-08-01 15:34:05 +0800

    inital INC basic class

--- include/inc/iinccontext.h
+++ include/inc/iinccontext.h
@@ -67,12 +67,6 @@ class IX_INC_EXPORT iINCContext : public iObject
 {
     IX_OBJECT(iINCContext)
 public:
-    /** A generic notification for operation completion */
-    struct Listener {
-        virtual ~Listener() = 0;
-        virtual void notify(iINCContext* c, iINCOperation* o) {}
-    };
-
     /** The state of a connection context */
     enum State {
         STATE_UNCONNECTED,    /**< The context hasn't been connected yet */
@@ -84,57 +78,7 @@ public:
         STATE_TERMINATED      /**< The connection was terminated cleanly */
     };
 
-    /** Some special flags for contexts. */
-    enum Flag {
-        FLAG_NOFLAGS = 0x0000U, /**< Flag to pass when no specific options are needed (used to avoid casting) */
-        FLAG__NOFAIL = 0x0002U  
-        /**< Don't fail if the daemon is not available when connect() is called, 
-          * instead enter STATE_CONNECTING state and wait for the daemon to appear. */
-    };
-    typedef uint Flags;
-
-    /** Subscription event mask, as used by pa_context_subscribe() */
-    enum SubscriptionMask {
-        SUBSCRIPTION_MASK_NULL = 0x0000U,           /**< No events */
-        SUBSCRIPTION_MASK_SINK = 0x0001U,           /**< Sink events */
-        SUBSCRIPTION_MASK_SOURCE = 0x0002U,         /**< Source events */
-        SUBSCRIPTION_MASK_SINK_INPUT = 0x0004U,     /**< Sink input events */
-        SUBSCRIPTION_MASK_SOURCE_OUTPUT = 0x0008U,  /**< Source output events */
-        SUBSCRIPTION_MASK_MODULE = 0x0010U,         /**< Module events */
-        SUBSCRIPTION_MASK_CLIENT = 0x0020U,         /**< Client events */
-        SUBSCRIPTION_MASK_SAMPLE_CACHE = 0x0040U,   /**< Sample cache events */
-        SUBSCRIPTION_MASK_SERVER = 0x0080U,         /**< Other global server changes. */
-        SUBSCRIPTION_MASK_ALL = SUBSCRIPTION_MASK_SINK | SUBSCRIPTION_MASK_SOURCE | SUBSCRIPTION_MASK_SINK_INPUT | SUBSCRIPTION_MASK_SOURCE_OUTPUT | \
-                                SUBSCRIPTION_MASK_MODULE | SUBSCRIPTION_MASK_CLIENT | SUBSCRIPTION_MASK_SAMPLE_CACHE | SUBSCRIPTION_MASK_SERVER
-        /**< Catch all events */
-    };
-    typedef uint SubscriptionMasks;
-
-    /** Subscription event types*/
-    enum SubscriptionEventType {
-        SUBSCRIPTION_EVENT_SINK = 0x0000U,          /**< Event type: Sink */
-        SUBSCRIPTION_EVENT_SOURCE = 0x0001U,        /**< Event type: Source */
-        SUBSCRIPTION_EVENT_SINK_INPUT = 0x0002U,    /**< Event type: Sink input */
-        SUBSCRIPTION_EVENT_SOURCE_OUTPUT = 0x0003U, /**< Event type: Source output */
-        SUBSCRIPTION_EVENT_MODULE = 0x0004U,        /**< Event type: Module */
-        SUBSCRIPTION_EVENT_CLIENT = 0x0005U,        /**< Event type: Client */
-        SUBSCRIPTION_EVENT_SAMPLE_CACHE = 0x0006U,  /**< Event type: Sample cache item */
-        SUBSCRIPTION_EVENT_SERVER = 0x0007U,        /**< Event type: Global server change, only occurring with SUBSCRIPTION_EVENT_CHANGE. */
-        SUBSCRIPTION_EVENT_CARD = 0x0009U,          /**< Event type: Card \since 0.9.15 */
-        SUBSCRIPTION_EVENT_FACILITY_MASK = 0x000FU, /**< A mask to extract the event type from an event value */
-        SUBSCRIPTION_EVENT_NEW = 0x0000U,           /**< A new object was created */
-        SUBSCRIPTION_EVENT_CHANGE = 0x0010U,        /**< A property of the object was modified */
-        SUBSCRIPTION_EVENT_REMOVE = 0x0020U,        /**< An object was removed */
-        SUBSCRIPTION_EVENT_TYPE_MASK = 0x0030U      /**< A mask to extract the event operation from an event value */
-    };
-
     iINCContext(iStringView name, iObject *parent = IX_NULLPTR);
-
-    /** Increase the reference count by one */
-    void ref();
-
-    /** Decrease the reference count by one */
-    void deref();
 
     /** Return the current context status */
     State getState() const;
@@ -145,21 +89,21 @@ public:
      * be notified when the connection is established. 
      * Returns negative on certain errors such as invalid state
      * or parameters. */
-    int connect(iStringView server, Flags flags);
+    int connect(iStringView server);
 
     /** Terminate the context connection immediately */
     void disconnect();
 
     /** Enable event notification */
-    iINCOperation* subscribe(SubscriptionMasks m, Listener* l);
+    iINCOperation* subscribe(uint32_t mask);
 
     /** Drain the context. If there is nothing to drain, the function returns NULL */
-    iINCOperation* drain(Listener* l);
+    iINCOperation* drain();
 
     /** Tell the daemon to exit. The returned operation is unlikely to
      * complete successfully, since the daemon probably died before
      * returning a success notification */
-    iINCOperation* exitDaemon(Listener* l);
+    iINCOperation* exitDaemon();
 
     /** Returns 1 when the connection is to a local daemon. Returns negative when no connection has been made yet. */
     int isLocal();
@@ -177,7 +121,7 @@ public:
 public: // signal
     void stateChanged(State st) ISIGNAL(stateChanged, st);
     void eventNotify(iStringView name) ISIGNAL(eventNotify, name);
-    void subscribeNotify(SubscriptionEventType t, xuint32 idx) ISIGNAL(subscribeNotify, t, idx);
+    void subscribeNotify(uint32_t event, xuint32 idx) ISIGNAL(subscribeNotify, event, idx);
 
 protected:
     virtual ~iINCContext();
